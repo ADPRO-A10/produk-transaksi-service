7b8d4869b5ac132970841f459b4ba2d1f7b3aafc
Dwiky Ahmad Megananta <119550129+DwikyAhmad@users.noreply.github.com>
2024-04-06 12:16:46 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/produktransaksiservice/ProdukTransaksiServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.produktransaksiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdukTransaksiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProdukTransaksiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/produktransaksiservice/ProdukTransaksiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.produktransaksiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdukTransaksiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
