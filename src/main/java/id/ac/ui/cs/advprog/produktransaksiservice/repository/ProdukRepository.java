80508da7d1e1a3587949083b3c4ec7e6a2d46121
Arya Lesmana <arya.lesmana21@ui.ac.id>
2024-04-24 20:52:57 +0700

    [RED] Add produkRepository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/produktransaksiservice/repository/ProdukRepository.java
@@ -0,0 +1,51 @@
+package id.ac.ui.cs.advprog.produktransaksiservice.repository;
+import id.ac.ui.cs.advprog.produktransaksiservice.model.Review;
+import org.hibernate.sql.exec.ExecutionException;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+import id.ac.ui.cs.advprog.produktransaksiservice.model.Produk;
+
+public class ProdukRepository {
+    ArrayList<Produk> listProduk = new ArrayList<>();
+    public Produk create(Produk createdProduk) {
+        listProduk.add(createdProduk);
+        return createdProduk;
+    }
+
+    public void deleteProduct(String id) {
+        for (int i = 0; i < listProduk.size(); i++) {
+            Produk product = listProduk.get(i);
+            if (product.getProdukId().equals(id)) {
+                listProduk.remove(product);
+            }
+        }
+    }
+    public Produk findByProductId(String id) {
+        if (listProduk != null) {
+            for (Produk product : listProduk) {
+                if (product.getProdukId().equals(id)) {
+                    return product;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Iterator<Produk> findAll() {
+        return listProduk.iterator();
+    }
+
+    public Produk editProduct(String id, Produk editedProduct) {
+        Produk targetProduct = findByProductId(id);
+        if (targetProduct != null) {
+            targetProduct.setStokTersedia(editedProduct.getStokTersedia());
+            targetProduct.setDeskripsi(editedProduct.getDeskripsi());
+            targetProduct.setHarga(editedProduct.getHarga());
+        }
+        return targetProduct;
+    }
+}
