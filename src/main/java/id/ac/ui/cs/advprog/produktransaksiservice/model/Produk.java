ca32eb67650402de8573d6113a478ddc6bc49f80
Arya Lesmana <arya.lesmana21@ui.ac.id>
2024-04-24 17:58:29 +0700

    [GREEN] Implement Produk model

--- src/main/java/id/ac/ui/cs/advprog/produktransaksiservice/model/Produk.java
+++ src/main/java/id/ac/ui/cs/advprog/produktransaksiservice/model/Produk.java
@@ -6,14 +6,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Getter @Setter
+@Getter
 public class Produk {
+    @Setter
     private String produkId = UUID.randomUUID().toString();
+
+    @Setter
     private String nama;
+
+    @Setter
     private int harga;
+
+    @Setter
     private String kategori;
+
+    @Setter
     private String deskripsi;
+
     private int stokTersedia;
+
+    @Setter
     private int stokTerjual;
+
+    @Setter
     private String penjual;
+
+    public void setStokTersedia(int stok) {
+        if (stok < 0) {
+            throw new IllegalArgumentException("Stok yang diinput tidak valid");
+        } else {
+            this.stokTersedia = stok;
+        }
+    }
 }
