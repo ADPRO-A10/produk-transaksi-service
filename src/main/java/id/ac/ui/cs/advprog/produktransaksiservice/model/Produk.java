2c5b49a701b4454742d0f44f0fd2238ab5f777b5
Arya Lesmana <arya.lesmana21@ui.ac.id>
2024-05-17 08:38:42 +0700

    [REFACTOR] Configure JpaRespository for Produk repository

--- src/main/java/id/ac/ui/cs/advprog/produktransaksiservice/model/Produk.java
+++ src/main/java/id/ac/ui/cs/advprog/produktransaksiservice/model/Produk.java
@@ -1,40 +1,53 @@
 package id.ac.ui.cs.advprog.produktransaksiservice.model;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Getter
+@Entity
+@Table(name = "Produk")
 public class Produk {
     @Setter
+    @Id
+    @Column(name = "Id")
     private String produkId = UUID.randomUUID().toString();
 
     @Setter
+    @Column(name = "Nama")
     private String nama;
 
     @Setter
+    @Column(name = "Harga")
     private int harga;
 
     @Setter
+    @Column(name = "Kategori")
     private String kategori;
 
     @Setter
+    @Column(name = "Deskripsi")
     private String deskripsi;
 
+    @Column(name = "StokTersedia")
     private int stokTersedia;
 
     @Setter
+    @Column(name = "StokTerjual")
     private int stokTerjual;
 
     @Setter
+    @Column(name = "Penjual")
     private String penjual;
 
     @Setter
+    @Transient
     private List<Review> reviews;
 
     public Produk() {
-
     }
 
     public void setStokTersedia(int stok) {
--- src/main/java/id/ac/ui/cs/advprog/produktransaksiservice/repository/ProdukRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/produktransaksiservice/repository/ProdukRepository.java
@@ -1,60 +1,15 @@
 package id.ac.ui.cs.advprog.produktransaksiservice.repository;
 import id.ac.ui.cs.advprog.produktransaksiservice.model.Review;
 import org.hibernate.sql.exec.ExecutionException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
-
+import org.springframework.data.repository.CrudRepository;
 import id.ac.ui.cs.advprog.produktransaksiservice.model.Produk;
 
-@Repository
-public class ProdukRepository {
-    ArrayList<Produk> listProduk = new ArrayList<>();
-    public Produk create(Produk createdProduk) {
-        String namaProdukYgDiinput = createdProduk.getNama();
-        for (Produk produk : listProduk) {
-            if (produk.getNama().equals(namaProdukYgDiinput)) {
-                throw new IllegalArgumentException("Produk dengan nama tersebut sudah ada");
-            }
-        }
-        listProduk.add(createdProduk);
-        return createdProduk;
-    }
-
-    public void deleteProduct(String id) {
-        for (int i = 0; i < listProduk.size(); i++) {
-            Produk product = listProduk.get(i);
-            if (product.getProdukId().equals(id)) {
-                listProduk.remove(product);
-            }
-        }
-    }
-    public Produk findByProductId(String id) {
-        if (listProduk != null) {
-            for (Produk product : listProduk) {
-                if (product.getProdukId().equals(id)) {
-                    return product;
-                }
-            }
-        }
-        return null;
-    }
-
-    public Iterator<Produk> findAll() {
-        return listProduk.iterator();
-    }
-
-    public Produk editProduct(String id, Produk editedProduct) {
-        Produk targetProduct = findByProductId(id);
-        if (targetProduct != null) {
-            targetProduct.setStokTersedia(editedProduct.getStokTersedia());
-            targetProduct.setDeskripsi(editedProduct.getDeskripsi());
-            targetProduct.setHarga(editedProduct.getHarga());
-            return targetProduct;
-        } else {
-            throw new IllegalArgumentException("Id produk tidak ditemukan");
-        }
-    }
+public interface ProdukRepository extends JpaRepository<Produk, String>{
 }
