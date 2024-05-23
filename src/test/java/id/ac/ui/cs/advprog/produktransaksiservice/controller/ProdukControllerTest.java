a1ead6e14ad4dfd2bcf78856e9472e484629bda1
Arya Lesmana <arya.lesmana21@ui.ac.id>
2024-05-24 01:21:27 +0700

    [RED] Add test for create produk with same name

--- src/test/java/id/ac/ui/cs/advprog/produktransaksiservice/controller/ProdukControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/produktransaksiservice/controller/ProdukControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,9 +26,6 @@ import static org.mockito.Mockito.*;
 public class ProdukControllerTest {
 
     @Mock
-    private ProdukRepository produkRepository;
-
-    @InjectMocks
     private ProdukServiceImpl produkServiceImpl;
 
     @InjectMocks
@@ -37,7 +35,6 @@ public class ProdukControllerTest {
 
     @BeforeEach
     void setup() {
-
     }
 
     @Test
@@ -51,13 +48,44 @@ public class ProdukControllerTest {
                 .stokTerjual(10)
                 .penjual("Rockstar Store")
                 .build();
-        when(produkRepository.save(produk)).thenReturn(produk);
 
+        when(produkServiceImpl.createProduk(produk)).thenReturn(produk);
         ResponseEntity<Produk> response = produkController.createProduk(produk);
 
         assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        assertEquals(produk, response.getBody());
+        assertEquals(produk.getProdukId(), response.getBody().getProdukId());
     }
+    @Test
+    void testCreateProdukWithSameName() {
+        Produk produk = produkBuilder.id("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3454")
+                .nama("Red Dead Redemption 2")
+                .kategori("Open World")
+                .harga(350000)
+                .deskripsi("Prequel to RDR 1")
+                .stokTersedia(100)
+                .stokTerjual(10)
+                .penjual("Rockstar Store")
+                .build();
+
+        when(produkServiceImpl.createProduk(produk)).thenReturn(produk);
+        ResponseEntity<Produk> response = produkController.createProduk(produk);
+
+        Produk produk2 = produkBuilder.id("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3454")
+                .nama("Red Dead Redemption 2")
+                .kategori("Open World")
+                .harga(350000)
+                .deskripsi("Prequel to RDR 1")
+                .stokTersedia(100)
+                .stokTerjual(10)
+                .penjual("Rockstar Store")
+                .build();
+
+        when(produkServiceImpl.createProduk(produk2)).thenReturn(null);
+        ResponseEntity<Produk> response2 = produkController.createProduk(produk);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response2.getStatusCode());
+    }
+
 
     @Test
     void testGetAllProduk() {
@@ -89,7 +117,7 @@ public class ProdukControllerTest {
         produkList.add(produk1);
         produkList.add(produk2);
 
-        when(produkRepository.findAll()).thenReturn(produkList);
+        when(produkServiceImpl.findAll()).thenReturn(produkList);
 
         ResponseEntity<List<Produk>> response = produkController.listProduk();
 
@@ -110,7 +138,7 @@ public class ProdukControllerTest {
                 .build();
 
         produkController.createProduk(produk);
-        when(produkRepository.findById("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3454")).thenReturn(Optional.of(produk));
+        when(produkServiceImpl.findProdukById("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3454")).thenReturn(produk);
 
         ResponseEntity<Produk> response = produkController.getProdukById("f8c3de3d-1fea-4d7c-a8b0-29f63c4c3454");
 
@@ -120,7 +148,7 @@ public class ProdukControllerTest {
 
     @Test
     void testGetProdukByIdNotFound() {
-        when(produkRepository.findById("123")).thenReturn(Optional.empty());
+        when(produkServiceImpl.findProdukById("123")).thenReturn(null);
         ResponseEntity<Produk> response = produkController.getProdukById("123");
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
@@ -137,9 +165,9 @@ public class ProdukControllerTest {
                 .stokTerjual(0)
                 .penjual("Seller A")
                 .build();
-        produkController.createProduk(produk);
-        when(produkRepository.findById("123")).thenReturn(Optional.of(produk));
-        assertNotNull(produkController.getProdukById("123"));
+
+        when(produkServiceImpl.createProduk(produk)).thenReturn(produk);
+        ResponseEntity<Produk> response = produkController.createProduk(produk);
 
         Produk updatedProduk = produkBuilder
                         .nama("Product B")
@@ -150,26 +178,16 @@ public class ProdukControllerTest {
                         .stokTerjual(5)
                         .penjual("Seller B")
                         .build();
-        when(produkRepository.save(updatedProduk)).thenReturn(updatedProduk);
 
-        ResponseEntity<Produk> response = produkController.editProduk("123", updatedProduk);
+        ResponseEntity<Produk> response2 = produkController.editProduk("123", updatedProduk);
 
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(updatedProduk, response.getBody());
+        assertEquals(HttpStatus.OK, response2.getStatusCode());
+        assertEquals(produk, response2.getBody());
     }
 
     @Test
     void testEditProdukNotFound() {
-        Produk updatedProduk = produkBuilder
-                        .nama("Product B")
-                        .harga(150)
-                        .kategori("Category B")
-                        .deskripsi("New Description")
-                        .stokTersedia(20)
-                        .stokTerjual(5)
-                        .penjual("Seller B")
-                        .build();
-        when(produkRepository.findById("123")).thenReturn(Optional.empty());
+        Produk updatedProduk = new Produk();
         ResponseEntity<Produk> response = produkController.editProduk("123", updatedProduk);
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
@@ -187,7 +205,7 @@ public class ProdukControllerTest {
                 .penjual("Seller A")
                 .build();
         produkController.createProduk(produk);
-        when(produkRepository.findById("123")).thenReturn(Optional.of(produk));
+        when(produkServiceImpl.findProdukById("123")).thenReturn(produk);
         assertNotNull(produkController.getProdukById("123"));
 
         ResponseEntity<Produk> response = produkController.deleteProduk("123");
@@ -197,7 +215,7 @@ public class ProdukControllerTest {
 
     @Test
     void testDeleteProdukNotFound() {
-        when(produkRepository.findById("123")).thenReturn(Optional.empty());
+        when(produkServiceImpl.findProdukById("123")).thenReturn(null);
         ResponseEntity<Produk> response = produkController.deleteProduk("123");
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
